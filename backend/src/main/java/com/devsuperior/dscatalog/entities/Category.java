330b476e6e331f0cbfe1d0a87069d4e7ac749302
Apaecida Rosa <lucita.rosa@gmail.com>
2021-05-18 16:29:48 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,11 +1,15 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +22,12 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE") //UTC
+	private Instant createdAt; //armazena dia e hora
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE") //UTC
+	private Instant updatedAt; //armazena dia e hora
 	
 	
 	public Category() { }
@@ -48,7 +58,26 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdte() {
+		updatedAt = Instant.now();
+	}
 
 	@Override
 	public int hashCode() {
