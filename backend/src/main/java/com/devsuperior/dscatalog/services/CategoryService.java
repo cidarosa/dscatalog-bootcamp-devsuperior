9aea54e02b92e8caa88a173b7dd95ad6320c3b18
Apaecida Rosa <lucita.rosa@gmail.com>
2021-05-19 18:02:02 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,15 +1,14 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,21 +24,31 @@ public class CategoryService {
 	// dependency
 	@Autowired
 	private CategoryRepository repository;
-
+	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list = repository.findAll();
-
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-
-		/*
-		 * List<CategoryDTO> listDto1 = new ArrayList<>(); for (Category cat : list) {
-		 * listDto1.add(new CategoryDTO(cat)); }
-		 * 
-		 * return listDto;
-		 */
-
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> list = repository.findAll(pageRequest);		
+		
+		return list.map(x -> new CategoryDTO(x));
 	}
+
+	// substituido para fazer paginação
+	/*
+	 * @Transactional(readOnly = true) public List<CategoryDTO> findAll() {
+	 * List<Category> list = repository.findAll();
+	 * 
+	 * return list.stream().map(x -> new
+	 * CategoryDTO(x)).collect(Collectors.toList());
+	 * 
+	 * 
+	 * List<CategoryDTO> listDto1 = new ArrayList<>(); for (Category cat : list) {
+	 * listDto1.add(new CategoryDTO(cat)); }
+	 * 
+	 * return listDto;
+	 * 
+	 * 
+	 * }
+	 */
 
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
