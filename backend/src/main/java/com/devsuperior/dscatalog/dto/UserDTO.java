f33d89f7c28b25197b2b995517e075465f3958dd
Apaecida Rosa <lucita.rosa@gmail.com>
2021-06-04 20:58:21 -0300

    CRUD User

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -0,0 +1,81 @@
+package com.devsuperior.dscatalog.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.devsuperior.dscatalog.entities.User;
+
+public class UserDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+//	private String password;
+	
+	Set<RoleDTO> roles = new HashSet<>();
+
+	public UserDTO() {
+	}
+
+	public UserDTO(Long id, String firstName, String lastName, String email){
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		
+	}
+	public UserDTO(User entity) {	
+		id = entity.getId();
+		firstName = entity.getFirstName();
+		lastName = entity.getLastName();
+		email = entity.getEmail();
+		//como fez o Fetch na classe USER - já vem os perfis ROLES
+		//pode carregar os perfis aqui
+		//povoar a lista de Role
+		entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+	}
+		
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<RoleDTO> getRoles() {
+		return roles;
+	}
+	
+	
+
+}
