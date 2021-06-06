18aa9e094d5a8f53d6d337944695eac4e0aa0bf9
Cida Castello <cidacastello.cursos@gmail.com>
2021-06-06 20:23:43 -0300

    Custom constraint validator

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dscatalog.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	//criando um método para verificar se já existe no DB
+	User findByEmail(String email);
 
 }
