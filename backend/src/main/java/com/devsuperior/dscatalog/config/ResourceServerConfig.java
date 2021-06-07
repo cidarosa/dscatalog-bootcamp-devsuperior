ab472ab53e2e72432ec57768c6ca37fd98e01bd5
Apaecida Rosa <lucita.rosa@gmail.com>
2021-06-07 19:18:34 -0300

    H2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,12 +16,17 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+	//liberar o H2
+	@Autowired
+	private Environment env;
+	
+	
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
 	//configurar as rotas
 	//GET
-	private static final String[] PUBLIC = {"/oauth/token"}; //todos podem acessar
+	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"}; //todos podem acessar
 	
 	//Rotas para operador ou admin
 	private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
@@ -33,6 +41,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		//liberando o H2
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
 		//configurando quem pode acessar o que
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
