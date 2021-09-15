04278491454cd11a1c97745d28371528b2b0a5b4
Aparecida Rosa <lucita.rosa@gmail.com>
2021-09-15 18:17:31 -0300

    Componente ProductCard

---
+++ frontweb/src/components/ProductCard/index.tsx
@@ -0,0 +1,23 @@
+import './styles.css';
+
+import ProductImg from 'assets/images/product.png';
+
+const ProductCard = () => {
+
+    return (
+        <div className="base-card product-card">
+            <div className="card-top-container">
+               <img src={ProductImg} alt="Nome do produto" />
+            </div>
+            <div className="card-bottom-container">
+                <h6>Nome do produto</h6>
+                <p>2346.67</p>
+            </div>
+
+        </div>
+
+    );
+
+}
+
+export default ProductCard;
