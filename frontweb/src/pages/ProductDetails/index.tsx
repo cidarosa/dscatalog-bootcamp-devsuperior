4b53e87f22db92b446cff9c2db09376bbbac38fd
Aparecida Rosa <lucita.rosa@gmail.com>
2021-09-27 19:15:06 -0300

    ProductDetails layout mobile

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,7 +1,45 @@
-const ProductDetails = () =>{
+import { ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
 
-    return(
-        <h1>Páigna ProductDetails</h1>
+import './styles.css';
+
+const ProductDetails = () => {
+
+    return (
+        <div className="product-details-container">
+            <div className="base-card product-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>VOLTAR</h2>
+                </div>
+
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do Produto" />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Nome do Produto</h1>
+                            <ProductPrice price= {2345.67}/>
+                        </div>
+                    </div>
+
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do Produto</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, assumenda?</p>
+
+                        </div>
+
+
+                    </div>
+                </div>
+
+
+            </div>
+
+
+        </div>
     );
 }
 
