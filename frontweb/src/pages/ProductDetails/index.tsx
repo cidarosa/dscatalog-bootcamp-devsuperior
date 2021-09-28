ca26c0653415be231ca7178f273005724a2abcdc
Aparecida Rosa <lucita.rosa@gmail.com>
2021-09-28 11:44:02 -0300

    Catalog - ProductDetaisl links

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,5 +1,7 @@
-import { ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
+
+import { Link } from "react-router-dom";
 
 import './styles.css';
 
@@ -8,10 +10,12 @@ const ProductDetails = () => {
     return (
         <div className="product-details-container">
             <div className="base-card product-details-card">
-                <div className="goback-container">
-                    <ArrowIcon />
-                    <h2>VOLTAR</h2>
-                </div>
+                <Link to="/products">
+                    <div className="goback-container">
+                        <ArrowIcon />
+                        <h2>VOLTAR</h2>
+                    </div>
+                </Link>
 
                 <div className="row">
                     <div className="col-xl-6">
@@ -20,7 +24,7 @@ const ProductDetails = () => {
                         </div>
                         <div className="name-price-container">
                             <h1>Nome do Produto</h1>
-                            <ProductPrice price= {2345.67}/>
+                            <ProductPrice price={2345.67} />
                         </div>
                     </div>
 
