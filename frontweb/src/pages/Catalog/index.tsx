2448a4f26e50ddad5eb165aed49dede7ae7626ae
Aparecida Rosa <lucita.rosa@gmail.com>
2021-09-29 18:49:19 -0300

    Pagination, styling

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,3 +1,4 @@
+import Pagination from "components/Pagination";
 import ProductCard from "components/ProductCard";
 import { Link } from "react-router-dom";
 import { Product } from "types/product";
@@ -27,9 +28,11 @@ const Catalog = () => {
 
     return (
         <div className="container my-4 catalog-container">
+
             <div className="row catalog-title-container">
                 <h1>Catálogo de Produtos</h1>
             </div>
+
             <div className="row">
                 <div className="col-sm-6 col-lg-4 col-xl-3">
                     <Link to="products/1">
@@ -61,6 +64,10 @@ const Catalog = () => {
                     </Link>
                 </div>
 
+            </div>
+
+            <div className="row">
+                <Pagination />
             </div>
 
         </div>
