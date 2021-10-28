b7942c1d9ff2df751d2dc3b03ae2064366a2e113
Aparecida Rosa <lucita.rosa@gmail.com>
2021-10-28 18:01:42 -0300

    Login request

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from "react-hook-form";
+import { requestBackendLogin } from 'util/requests';
 
 import './styles.css';
 
@@ -14,7 +15,14 @@ const Login = () => {
     const { register, handleSubmit } = useForm<FormData>();
 
     const onSubmit = (formData : FormData) => {
-        console.log(formData);
+        requestBackendLogin(formData)
+        .then(response => {
+            console.log('SUCESSO', response);
+        })
+        .catch(error => {
+            console.log('ERRO', error);
+        });
+        
     };
 
     return (
