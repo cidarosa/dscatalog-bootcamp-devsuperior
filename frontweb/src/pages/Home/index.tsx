45f37613afff2ac2c3413e0273a239fef24c0ac1
Aparecida Rosa <lucita.rosa@gmail.com>
2021-11-05 17:08:11 -0300

    PrivateRoute

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -8,9 +8,6 @@ import {isAuthenticated } from 'util/requests';
 const Home = () => {
     return (
         <div className="home-container">
-
-            <h1>{isAuthenticated() ? 'autenticado' : 'NÃO autenticado'}</h1>
-
             <div className="base-card home-card">
                 <div className="home-content-container">
                     <div>
