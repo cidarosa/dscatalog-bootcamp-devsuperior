03eb9e1380fb5e415a59222c4406ef50566bd007
Aparecida Rosa <lucita.rosa@gmail.com>
2021-11-11 18:00:17 -0300

    hasAnyRoles

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -3,11 +3,12 @@ import './styles.css';
 
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
-import {isAuthenticated } from 'util/requests';
+import {hasAnyRoles } from 'util/requests';
 
 const Home = () => {
     return (
         <div className="home-container">
+            <h1>Resultado = {hasAnyRoles(['ROLE_ADMIN']) ? 'sim' : 'não'}</h1>
             <div className="base-card home-card">
                 <div className="home-content-container">
                     <div>
