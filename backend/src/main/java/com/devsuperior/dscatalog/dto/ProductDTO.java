1cff389c2ece72c1edd2dd577cd1731900ee3f8b
Cida Rosa <lucita.rosa@gmail.com>
2022-03-30 11:55:27 -0300

    Product Save

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
@@ -23,16 +24,20 @@ public class ProductDTO implements Serializable {
 	@Size(min = 5, max = 60, message = "Deve ter entre 5 e 60 caracteres")
 	@NotBlank(message = "Campo requerido")
 	private String name;
+	
+	@NotBlank(message = "Campo requerido")
 	private String description;
 	
 	@Positive(message = "O preço deve ser um valor positivo")
 	private Double price;
+	
 	private String imgUrl;
 	
 	@PastOrPresent(message = "A data do produto não pode ser futura")
 	private Instant date;
 	
 	//permite ter as categorias
+	@NotEmpty(message = "Produto sem categoria não é permitido")
 	private List<CategoryDTO> categories = new ArrayList<>();
 	
 	public ProductDTO() { }
--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,41 +1,116 @@
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { Product } from 'types/product';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
 const Form = () => {
+  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Product>();
+
+  const onSubmit = (formData: Product) => {
+    const data = {
+      ...formData,
+      imgUrl:
+        'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',
+      categories: [{ id: 1, name: '' }],
+    };
+
+    /* configuração da requesição para salvar o Product */
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/products',
+      //data: formData, //dados que serão passados
+      // data: data, //não precisa colocar qdo o nome da variável é igual ao atributo
+      data,
+      withCredentials: true,
+      /* tem que estar autenticado para passar a requisção */
+    };
+
+    requestBackend(config).then((response) => {
+      console.log(response.data);
+    });
+  };
+
+  const handleCancel = () => {
+    history.push('/admin/products');
+  };
+
   return (
     //pega do style do List
     <div className="product-crud-container">
       <div className="base-card product-crud-form-card">
         <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row product-crud-inputs-container">
             <div className="col-lg-6 product-crud-inputs-left-container">
               <div className="margin-bottom-30">
-                <input className="form-control base-input" type="text" />
+                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  } `}
+                  placeholder="Nome do Produto"
+                  name="name"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.name?.message}
+                </div>
               </div>
 
               <div className="margin-bottom-30">
-                <input className="form-control base-input" type="text" />
-              </div>
-
-              <div>
-                <input className="form-control base-input" type="text" />
+                <input
+                  {...register('price', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.price ? 'is-invalid' : ''
+                  } `}
+                  placeholder="Preço"
+                  name="price"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.price?.message}
+                </div>
               </div>
             </div>
 
             <div className="col-lg-6">
               <div>
                 <textarea
-                  name=""
                   rows={10}
-                  className="form-control base-input h-auto"
+                  {...register('description', {
+                    required: 'Campo obrigatório',
+                  })}
+                  className={`form-control base-input h-auto ${
+                    errors.description ? 'is-invalid' : ''
+                  } `}
+                  placeholder="Descrição"
+                  name="description"
                 />
+                <div className="invalid-feedback d-block">
+                  {errors.description?.message}
+                </div>
               </div>
             </div>
           </div>
 
           <div className="product-crud-buttons-container">
-            <button className="btn btn-outline-danger product-crud-button">
+            <button
+              className="btn btn-outline-danger product-crud-button"
+              onClick={handleCancel}
+            >
               CANCELAR
             </button>
             <button className="btn btn-primary product-crud-button text-white">
