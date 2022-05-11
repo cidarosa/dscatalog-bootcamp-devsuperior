c63a29a54a6d803988545d69bbc818d90b1c853c
Cida <96838817+lucitacastello@users.noreply.github.com>
2022-05-11 16:34:53 -0300

    REFACTOR token, role

--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -1,12 +1,47 @@
 import { formatPrice } from 'util/formatters';
 
-test('formatPrice should format number pt-BR when given 10.1', () => {
-  // ARRANGE
-  const value = 10.1;
+describe('formatPrice for positive numbers', () => {
+  test('formatPrice should format number pt-BR when given 10.1', () => {
+    // ARRANGE
+    const value = 10.1;
 
-  // ACT
-  const result = formatPrice(value);
+    // ACT
+    const result = formatPrice(value);
 
-  // ASSERT
-  expect(result).toEqual('10,10');
+    // ASSERT
+    expect(result).toEqual('10,10');
+  });
+
+  test('formatPrice should format number pt-BR when given 0.1', () => {
+    // ARRANGE
+
+    // ACT
+    const result = formatPrice(0.1);
+
+    // ASSERT
+    expect(result).toEqual('0,10');
+  });
+});
+
+describe('formatPrice for non-positive numbers', () => {
+  test('formatPrice should format number pt-BR when given 0', () => {
+    // ARRANGE
+    const value = 0;
+
+    // ACT
+    const result = formatPrice(value);
+
+    // ASSERT
+    expect(result).toEqual('0,00');
+  });
+
+  test('formatPrice should format number pt-BR when given -5,1', () => {
+    // ARRANGE
+
+    // ACT
+    const result = formatPrice(-5.1);
+
+    // ASSERT
+    expect(result).toEqual('-5,10');
+  });
 });
