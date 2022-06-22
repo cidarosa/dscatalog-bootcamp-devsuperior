f90efaadcf88a33c128858200ff08587dad8374a
Cida <96838817+lucitacastello@users.noreply.github.com>
2022-06-22 14:32:30 -0300

    Catalog, waitFor, async-await

---
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import history from 'util/history';
+import Catalog from '..';
+
+test('should render Catalog with products', async () => {
+  render(
+    <Router history={history}>
+      <Catalog />
+    </Router>
+  );
+
+  screen.debug();
+
+  expect(screen.getByText('Catálogo de Produtos')).toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(screen.getByText('Smart TV')).toBeInTheDocument();
+  });
+
+  //screen.debug();
+
+  
+});
